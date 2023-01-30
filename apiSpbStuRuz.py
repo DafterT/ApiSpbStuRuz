4c2f38e6a910469ad95a31097a578ffcd4394526
Dafter <dafterplay@mail.ru>
2023-01-30 08:48:01 +0300

    corrected logs and add check on None from __get_response_json

--- apiSpbStuRuz.py
+++ apiSpbStuRuz.py
@@ -36,22 +36,25 @@ class ApiSpbStuRuz:
                     return None
         except ClientConnectionError as e:
             # Ошибка клиента при запросе на сервер
-            self._logger.exception(f'Can\'t connect to the server: {e}')
+            self._logger.error(f'Can\'t connect to the server: {e}')
             return None
 
     async def get_faculties(self) -> [dataClasses.Faculty]:
         self._logger.debug(f'Try to get faculties')
         faculties_json = await self.__get_response_json(apiPaths.faculties)
         self._logger.debug(f'Information about faculties: {faculties_json}')
+        if faculties_json is None:
+            self._logger.error(f'Returned faculties_json is None')
+            return None
         if 'faculties' not in faculties_json:
-            self._logger.exception(f'Can\'t found "faculties" in json_file')
+            self._logger.error(f'Can\'t found "faculties" in json_file')
             return None
         try:
             faculties_list = [dataClasses.Faculty(**item) for item in faculties_json['faculties']]
             self._logger.debug(f'Information about faculties in list: {faculties_list}')
             return faculties_list
         except TypeError as e:
-            self._logger.exception(f'Can\'t convert to Faculty: {e}')
+            self._logger.error(f'Can\'t convert to Faculty: {e}')
             return None
 
     async def __aexit__(self, *err):
